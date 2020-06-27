7a66907b6db920b4ba03d10b62fb07b7925d7855
OlushkaZ <gincgo@yandex.ru>
2020-06-27 22:55:00 +0300

    state, hoc, audio

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -11,7 +11,6 @@ export default [
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `blues`,
     }, {
-      // src: `https://commons.wikimedia.org/wiki/File:Ot_D_-_Marko_Todorka_zalubi_-_melodia_gadulka.ogg`,
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `jazz`,
     }, {
@@ -25,14 +24,14 @@ export default [
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
     },
     answers: [{
-      picture: `${AVATAR_URL}/${Math.random()}`,
+      picture: `${AVATAR_URL}/A`,
       artist: `John Snow`,
     }, {
-      picture: `${AVATAR_URL}/${Math.random()}`,
+      picture: `${AVATAR_URL}/AB`,
       artist: `Jack Daniels`,
     }, {
-      picture: `${AVATAR_URL}/${Math.random()}`,
+      picture: `${AVATAR_URL}/AC`,
       artist: `Jim Beam`,
     }],
-  }
+  },
 ];
